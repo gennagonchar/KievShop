ec590ee93fc908da2383f83196318cb364814410
genadiy <gennagonchar@gmail.com>
2018-01-01 01:53:39 +0200

    First commit

---
+++ include/block-header.php
@@ -0,0 +1,16 @@
+<div id="block-header">
+
+    <div id="header-top-block">
+        <ul id="heder-top-menu">
+            <li>Наш город - Киев</li>
+            <li><a href="o-nas.php">О нас</a></li>
+            <li><a href="magaziny.php">Магазины</a></li>
+            <li><a href="contacts.php">Контакты</a></li>
+
+
+        </ul>
+
+    </div>
+
+
+</div>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <link href="css/style.css" rel="stylesheet" type="text/css"/>
+    <title>Урок Интернет Магазин </title>
+</head>
+<body>
+<div id="block-body">
+
+<?php
+include ("include/block-header.php")
+?>
+
+<div id="block-right"></div>
+
+<div id="block-content"></div>
+
+<?php
+include ("include/footer.php")
+?>
+
+</div>
+
+</body>
+</html>
